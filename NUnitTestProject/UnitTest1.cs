29e9215c74653bc8e8ca86ad6baa790393ca6cde
himanshunete <himanshuneteh@gmail.com>
2021-02-15 13:43:16 +0530

    [himanshunete]Add EmptyQueueUsingDequeue

--- NUnitTestProject/UnitTest1.cs
+++ NUnitTestProject/UnitTest1.cs
@@ -43,6 +43,7 @@ namespace NUnitTestProject
             MyNode pop2 = myStack.Pop();
             Assert.AreEqual(null, pop2.getNext());
         }
+
         /// <summary>
         /// TC-3 Test to check Queue
         /// </summary>
@@ -61,6 +62,26 @@ namespace NUnitTestProject
             peak = myQueue.Peak();
             Assert.AreEqual(myFirstNode, peak);
 
+        }
+
+        /// <summary>
+        /// TC-4 Test to empty queue 
+        /// </summary>
+        [Test]
+        public void Given3Number_WhenDequeue_ShouldReturnEmptyQueue()
+        {
+
+            MyNode myFirstNode = new MyNode(56);
+            MyNode mySecondNode = new MyNode(30);
+            MyNode myThirdNode = new MyNode(70);
+            MyQueue myQueue = new MyQueue();
+            myQueue.Enqueue(myFirstNode);
+            myQueue.Enqueue(mySecondNode);
+            myQueue.Enqueue(myThirdNode);
+            myQueue.Dequeue();
+            myQueue.Dequeue();
+            MyNode dequeue2 = myQueue.Dequeue();
+            Assert.AreEqual(null, dequeue2.getNext());
         }
     }
 }
--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -7,25 +7,22 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to Stack");
-            MyNode myFirstNode = new MyNode(70);
+            Console.WriteLine("Welcome to Queue");
+            MyNode myFirstNode = new MyNode(56);
             MyNode mySecondNode = new MyNode(30);
-            MyNode myThirdNode = new MyNode(56);
-            MyStack myStack = new MyStack();
-            myStack.Push(myFirstNode);
-            myStack.Push(mySecondNode);
-            myStack.Push(myThirdNode);
-            myStack.PrintStack();
-            MyNode pop = myStack.Pop();
-            // First Element is removed
-            myStack.PrintMyNodes();
-            MyNode pop1 = myStack.Pop();
-            // Second Element is removed
-            myStack.PrintMyNodes();
-            MyNode pop2 = myStack.Pop();
-            if ( pop2.getNext() == null )
+            MyNode myThirdNode = new MyNode(70);
+            MyQueue myQueue = new MyQueue();
+            myQueue.Enqueue(myFirstNode);
+            myQueue.Enqueue(mySecondNode);
+            myQueue.Enqueue(myThirdNode);
+            myQueue.Dequeue();
+            myQueue.PrintQueue();
+            myQueue.Dequeue();
+            myQueue.PrintQueue();
+            MyNode dequeue2 = myQueue.Dequeue();
+            if ( dequeue2.getNext() == null )
             {
-                Console.WriteLine("Stack is empty");
+                Console.WriteLine("Queue is empty");
             }         
         }
     }
